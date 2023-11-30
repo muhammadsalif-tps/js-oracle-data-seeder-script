c0bf36fa24208f623cda63476dcc3748e14cad18
muhammadsalif-tps <muhammadsalif.tps@gmail.com>
2023-11-30 16:12:16 +0500

    [update] fake data insert implementation.

--- index.js
+++ index.js
@@ -17,68 +17,68 @@ oracledb.getConnection(dbConfig).then((_db) => {
 const processData = () => {
   const inputData = {
     id: faker.number.int({ min: 10, max: 100 }),
-    order_id: faker.string.uuid(),
-    transaction_id: faker.string.uuid(),
-    transaction_type: faker.string.uuid(),
-    service_number: faker.string.uuid(),
-    customerid: faker.string.uuid(),
-    qidcrn: faker.string.uuid(),
-    account_number: faker.string.uuid(),
-    email_address: faker.string.uuid(),
-    ip_address: faker.string.uuid(),
-    channel: faker.string.uuid(),
-    subchannel: faker.string.uuid(),
-    service_type: faker.string.uuid(),
-    amount: faker.string.uuid(),
-    commission_amount: faker.string.uuid(),
-    vat: faker.string.uuid(),
-    status: faker.string.uuid(),
-    transaction_status: faker.string.uuid(),
-    bank_status: faker.string.uuid(),
-    fullfillment_responsecode: faker.string.uuid(),
-    fullfillment_status: faker.string.uuid(),
-    bank_description: faker.string.uuid(),
-    pg_description: faker.string.uuid(),
+    order_id: faker.string.sample({ min: 5, max: 6 }),
+    transaction_id: faker.string.sample({ min: 5, max: 6 }),
+    transaction_type: faker.string.sample({ min: 5, max: 6 }),
+    service_number: faker.string.sample({ min: 5, max: 6 }),
+    customerid: faker.string.sample({ min: 5, max: 6 }),
+    qidcrn: faker.string.sample({ min: 5, max: 6 }),
+    account_number: faker.string.sample({ min: 5, max: 6 }),
+    email_address: faker.string.sample({ min: 5, max: 6 }),
+    ip_address: faker.string.sample({ min: 5, max: 6 }),
+    channel: faker.string.sample({ min: 5, max: 6 }),
+    subchannel: faker.string.sample({ min: 5, max: 6 }),
+    service_type: faker.string.sample({ min: 5, max: 6 }),
+    amount: faker.string.sample({ min: 5, max: 6 }),
+    commission_amount: faker.string.sample({ min: 5, max: 6 }),
+    vat: faker.string.sample({ min: 5, max: 6 }),
+    status: faker.string.sample({ min: 5, max: 6 }),
+    transaction_status: faker.string.sample({ min: 5, max: 6 }),
+    bank_status: faker.string.sample({ min: 5, max: 6 }),
+    fullfillment_responsecode: faker.string.sample({ min: 5, max: 6 }),
+    fullfillment_status: faker.string.sample({ min: 5, max: 6 }),
+    bank_description: faker.string.sample({ min: 5, max: 6 }),
+    pg_description: faker.string.sample({ min: 5, max: 6 }),
     payment_datetime: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2030-01-01T00:00:00.000Z' }), // '2026-05-16T02:22:53.002Z'
     fullfillment_datetime: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2030-01-01T00:00:00.000Z' }), // '2026-05-16T02:22:53.002Z'
     refund_datetime: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2030-01-01T00:00:00.000Z' }), // '2026-05-16T02:22:53.002Z'
     orphan_datetime: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2030-01-01T00:00:00.000Z' }), // '2026-05-16T02:22:53.002Z'
-    pgw_payment_type: faker.string.uuid(),
-    payment_mode: faker.string.uuid(),
-    card_number: faker.string.uuid(),
-    card_scheme: faker.string.uuid(),
-    card_expiry: faker.string.uuid(),
-    card_holdername: faker.string.uuid(),
-    issuer_bank: faker.string.uuid(),
-    dpan: faker.string.uuid(),
-    funding_method: faker.string.uuid(),
-    mid: faker.string.uuid(),
-    merchant_id: faker.string.uuid(),
-    sub_merchant_id: faker.string.uuid(),
-    currency: faker.string.uuid(),
-    firstname: faker.string.uuid(),
-    lastname: faker.string.uuid(),
-    rrn: faker.string.uuid(),
-    stan: faker.string.uuid(),
-    acquirer_code: faker.string.uuid(),
-    authcode: faker.string.uuid(),
-    card_acceptor_terminal_id: faker.string.uuid(),
-    inserted_by: faker.string.uuid(),
-    modified_by: faker.string.uuid(),
+    pgw_payment_type: faker.string.sample({ min: 5, max: 6 }),
+    payment_mode: faker.string.sample({ min: 5, max: 6 }),
+    card_number: faker.string.sample({ min: 5, max: 6 }),
+    card_scheme: faker.string.sample({ min: 5, max: 6 }),
+    card_expiry: faker.string.sample({ min: 5, max: 6 }),
+    card_holdername: faker.string.sample({ min: 5, max: 6 }),
+    issuer_bank: faker.string.sample({ min: 5, max: 6 }),
+    dpan: faker.string.sample({ min: 5, max: 6 }),
+    funding_method: faker.string.sample({ min: 5, max: 6 }),
+    mid: faker.string.sample({ min: 5, max: 6 }),
+    merchant_id: faker.string.sample({ min: 5, max: 6 }),
+    sub_merchant_id: faker.string.sample({ min: 5, max: 6 }),
+    currency: faker.string.sample({ min: 1, max: 3 }),
+    firstname: faker.string.sample({ min: 5, max: 6 }),
+    lastname: faker.string.sample({ min: 5, max: 6 }),
+    rrn: faker.string.sample({ min: 5, max: 6 }),
+    stan: faker.string.sample({ min: 5, max: 6 }),
+    acquirer_code: faker.string.sample({ min: 5, max: 6 }),
+    authcode: faker.string.sample({ min: 5, max: 6 }),
+    card_acceptor_terminal_id: faker.string.sample({ min: 5, max: 6 }),
+    inserted_by: faker.string.sample({ min: 5, max: 6 }),
+    modified_by: faker.string.sample({ min: 5, max: 6 }),
     initiate_datetime: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2030-01-01T00:00:00.000Z' }), // '2026-05-16T02:22:53.002Z'
     modified_datetime: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2030-01-01T00:00:00.000Z' }), // '2026-05-16T02:22:53.002Z'
-    instrument_type: faker.string.uuid(),
-    response_code: faker.string.uuid(),
-    stage1_status: faker.string.uuid(),
-    stage2_status: faker.string.uuid(),
-    stage3_status: faker.string.uuid(),
-    stage4_status: faker.string.uuid(),
+    instrument_type: faker.string.sample({ min: 5, max: 6 }),
+    response_code: faker.string.sample({ min: 5, max: 6 }),
+    stage1_status: faker.string.sample({ min: 5, max: 6 }),
+    stage2_status: faker.string.sample({ min: 5, max: 6 }),
+    stage3_status: faker.string.sample({ min: 5, max: 6 }),
+    stage4_status: faker.string.sample({ min: 5, max: 6 }),
     stage1_status_datetime: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2030-01-01T00:00:00.000Z' }), // '2026-05-16T02:22:53.002Z'
     stage2_status_datetime: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2030-01-01T00:00:00.000Z' }), // '2026-05-16T02:22:53.002Z'
     stage3_status_datetime: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2030-01-01T00:00:00.000Z' }), // '2026-05-16T02:22:53.002Z'
-    stage4_status_datetim: faker.string.uuid()
+    stage4_status_datetime: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2030-01-01T00:00:00.000Z' }) // '2026-05-16T02:22:53.002Z'
   };
-  const sql = `INSERT INTO IRIS_CUSTOM.CHANNEL_WISE_TRANSACTION_DAY 
+  const sql = `INSERT INTO IRIS_CUSTOM.ODS_TRANSACTION_LOG_2 
               (
                 ID,
                 ORDER_ID,
@@ -144,6 +144,7 @@ const processData = () => {
               )
               VALUES 
               (
+                :id,
                 :order_id,
                 :transaction_id,
                 :transaction_type,
@@ -179,7 +180,9 @@ const processData = () => {
                 :issuer_bank,
                 :dpan,
                 :funding_method,
+                :mid,
                 :merchant_id,
+                :sub_merchant_id,
                 :currency,
                 :firstname,
                 :lastname,
@@ -188,19 +191,26 @@ const processData = () => {
                 :acquirer_code,
                 :authcode,
                 :card_acceptor_terminal_id,
-                :inserted_by
+                :inserted_by,
+                :modified_by,
+                :initiate_datetime,
+                :modified_datetime,
+                :instrument_type,
+                :response_code,
+                :stage1_status,
+                :stage2_status,
+                :stage3_status,
+                :stage4_status,
+                :stage1_status_datetime,
+                :stage2_status_datetime,
+                :stage3_status_datetime,
+                :stage4_status_datetime               
               )`;
-
   const binds = { ...inputData };
-  // return dbInstance.execute(sql, binds, { autoCommit: true });
-  return new Promise((resolve, reject) => {
-    console.log("SQL", sql);
-    console.log("DATA", inputData);
-    resolve()
-  });
+  return dbInstance.execute(sql, binds, { autoCommit: true });
 }
 
-async function middleFunction(totalRecords = 100000000, maxRequest = 100, currentRecursionIteration = 1, failedRequests = []) {
+async function middleFunction(totalRecords = 100000, maxRequest = 10, currentRecursionIteration = 1, failedRequests = []) {
   try {
     const recordsToDump = Math.min(maxRequest, totalRecords);
     const _promises = []
@@ -214,8 +224,9 @@ async function middleFunction(totalRecords = 100000000, maxRequest = 100, curren
       }));
     }
 
-    const result = await Promise.all(_promises);
-    console.log("result", result);
+    await Promise.all(_promises);
+    console.log("currentRecursionIteration", currentRecursionIteration);
+    console.log("DUMP==>", recordsToDump);
     totalRecords = Math.abs(totalRecords - recordsToDump);
 
     if (totalRecords) {
